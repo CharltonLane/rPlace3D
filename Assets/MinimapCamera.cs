17ba3c5e140db07c528be9b72bae35021100355a
Charlton Lane <charlton.lane14@gmail.com>
2022-04-10 11:16:31 +1000

    Added minimap zooming and snoo image to the corner
    Changed the skybox  and walls around the map to the same grey from rPlace

--- Assets/MinimapCamera.cs
+++ Assets/MinimapCamera.cs
@@ -14,10 +14,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MinimapCameraPositionUpdater : MonoBehaviour {
+public class MinimapCamera : MonoBehaviour {
 
     [SerializeField] private Transform _parent;
     [SerializeField] private Camera _camera;
+    private float _maxZoom = 24;
+    private float _minZoom = 1;
+    private float _currentZoom = 10;
+    private float _zoomSensitiviy = 30;
 
     void Update() {
         if (Input.GetKeyDown(KeyCode.M)) {
@@ -25,6 +29,11 @@ public class MinimapCameraPositionUpdater : MonoBehaviour {
         }
 
         transform.position = new Vector3(_parent.position.x, 1000, _parent.position.z);
+
+        // Minimap zooming.
+        _currentZoom -= Input.mouseScrollDelta.y * Time.deltaTime * _zoomSensitiviy;
+        _currentZoom = Mathf.Clamp(_currentZoom, _minZoom, _maxZoom);
+        _camera.orthographicSize = _currentZoom;
     }
 }
 
